1d4b64b21df162b17cb41de45025d1d53c1baffb
Jamie Robson <jamie.robson@3shape.com>
2021-01-04 13:53:20 +0100

    I really am trying here.
    The user is set
    The login screen is not, even though I only log on as one user

--- Keyboard/Resetting culture and language settings after a windows update.ps1
+++ Keyboard/Resetting culture and language settings after a windows update.ps1
@@ -1,14 +1,31 @@
-﻿#http://docwiki.embarcadero.com/RADStudio/Sydney/en/Language_Culture_Names,_Codes,_and_ISO_Values
+﻿## http://docwiki.embarcadero.com/RADStudio/Sydney/en/Language_Culture_Names,_Codes,_and_ISO_Values
 
-$defaultLocate = 'da'
-$desiredLocales = $($defaultLocate, 'en-GB', 'da')
+class LanguageSetting
+{
+    [string] $Locale;
+    [string] $GeoId;
+
+    LanguageSetting([string]$locale, [string]$geoId)
+    {
+        $this.Locale = $locale;
+        $this.GeoId = $geoId;
+    }
+}
+
+$uk = [LanguageSetting]::new("en-GB", "0xf2");
+$denmark = [LanguageSetting]::new("da", "0x3d");
+
+$desiredLocales = $($uk, $denmark)
+
 
 $languageList = Get-WinUserLanguageList;
 $languageList.Clear();
-$desiredLocales | ForEach-Object {
+
+$desiredLocales.Locate | ForEach-Object {
     $languageList.Add($_);
 }
 
+Set-WinHomeLocation -GeoId $denmark.GeoId;
+Set-WinUILanguageOverride -Language $uk.Locale
 Set-WinUserLanguageList $languageList
-
 Get-WinUserLanguageList
