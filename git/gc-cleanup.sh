a907eacd19d12c254c0f18ba3a7da775f558c261
Jamie Robson <jamie.robson@3shape.com>
2021-03-25 13:09:19 +0100

    Add "main" friendly nuget aliases

--- git/gc-cleanup.sh
+++ git/gc-cleanup.sh
@@ -1,34 +1,45 @@
-skipSubfoldersOfTheseFolders=()
+skipSubfoldersOfTheseFolders=(_3s)
 
-function firstStringContainsSecond {
-	string="$1"
-    substring="$2"
-	local contains=false
-	if test "${string#*$substring}" != "$string" 
-	then
-		contains=true
-	fi
-	echo $contains
+function collectGarbage {
+	git gc --quiet \
+	&& git gc --quiet --aggressive \
+	&& git gc --quiet --prune=now
 }
 
-function arrayContains {
-	skipTheseFolders=("_3s")
-    matchThis="$1"
-	local contains=false
+function cleanSingleBranchIfStale {
+	oneMonthAgo=$(date -d "-1 month" +%s)
+	commitThenDate=(${1//::/ })
+	branch=${commitThenDate[0]}
+	lastUpdated=$(date -d ${commitThenDate[1]} +%s) 
 	
-	for folder in "${skipTheseFolders[@]}"
+	if [ "$branch" == "master" ] || [ "$branch" == "main" ]; then
+		return 0
+	fi
+	
+	if [ "$lastUpdated" -gt "$oneMonthAgo" ]; then
+		return 0
+	fi
+		# Branch is recent enough
+		echo "Branch $branch is stale. Removing."
+	
+	currentlyCheckedOut=$(git branch --show-current)
+	if [ "$branch" == "$currentlyCheckedOut" ]; then
+		git checkout master
+	fi
+
+	git branch -D $branch
+}
+
+function cleanAllStaleBranches {
+	branches=$(git for-each-ref --sort=committerdate refs/heads/ --format='%(refname:short)::%(committerdate:short)')
+	for branch in $branches
 	do
-		if test "${folder#*$matchThis}" != "$folder" 
-		then
-			contains=true
-		fi
-		echo $contains
+		cleanSingleBranchIfStale $branch
 	done
 }
 
-function gitCollectGarbage {
-	requestedFolder="$1"    
-	
+function cleanRepo {
+	requestedFolder=$1
 	for folder in "${skipSubfoldersOfTheseFolders[@]}"
 	do
 		if test "${requestedFolder#*$folder}" != "$requestedFolder" 
@@ -37,15 +48,18 @@ function gitCollectGarbage {
 			return 0
 		fi
 	done
-	
-	cd "$1" \
-	&& pwd \
-	&& git gc --quiet\
-	&& git gc --quiet --aggressive \
-	&& git gc --quiet --prune=now \
-	
+
+	cd $1
+	pwd
+	cleanAllStaleBranches
+	collectGarbage
 }
 
 cd c:\source
-for D in ./*/; do (gitCollectGarbage $D) done
-for D in ./_*/*/; do (gitCollectGarbage $D) done
+for repoFolder in ./*/; do (cleanRepo $repoFolder) done
+for repoFolder in ./_*/*/; do (cleanRepo $repoFolder) done
+
+read -t 3 -n 1
+if [ $? = 0 ] ; then
+exit ;
+fi
--- nuget/generate-nuget-config.ps1
+++ nuget/generate-nuget-config.ps1
@@ -15,7 +15,7 @@ function Get-UserPromptText {
     
     Append-LineWithSpacing $stringBuilder "Enter your personal access token";
     Append-LineWithSpacing $stringBuilder "To generate your personal access token: ";
-    Append-LineWithSpacing $stringBuilder "1. Open https://dev.azure.com/3Shape/_usersSettings/tokens";
+    Append-LineWithSpacing $stringBuilder "1. Open the user settings > tokens menu on devops";
     Append-LineWithSpacing $stringBuilder "2. Add a new token, with the scope PACKAGE READ only, and and appropriate expiry. The shorter this is, the more often you'll have to repeat this process";
     Append-LineWithSpacing $stringBuilder "3. Copy the token, and paste it below. If you navigate away from the page before copying it, you'll never be able to retrieve the secret again";
     Append-LineWithSpacing $stringBuilder "*** NOTE: If you navigate away from the page before copying it, you'll never be able to retrieve the secret again, and will have to regenerate the token ***";
